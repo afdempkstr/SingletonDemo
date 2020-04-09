226afb9a336f7f2691b54f74ca9ffd93f3f81452
Kostas Stroggylos <afdempkstr@users.noreply.github.com>
2020-04-09 18:41:25 +0300

    Initial singleton implementation

---
+++ SingletonDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tpm = TaxPayerManager.Instance;
+
+            var tmp2 = TaxPayerManager.Instance;
+
+            Console.WriteLine(tmp2 == tpm);
+            Console.Read();
+        }
+    }
+}
---
+++ SingletonDemo/TaxPayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace SingletonDemo
+{
+    public class TaxPayerManager
+    {
+        private static TaxPayerManager _instance;
+        private static object syncLock = new object();
+
+        public static TaxPayerManager Instance
+        {
+            get
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new TaxPayerManager();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private List<string> _taxPayers;
+
+        private TaxPayerManager()
+        {
+            _taxPayers = new List<string>();
+        }
+
+        public void AddTaxPayer(string name)
+        { }
+
+    }
+}
